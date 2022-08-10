07393ea9e3caba01f1ae17de3ff122906d9bc590
Juani Testa <Juani.testa@hotmail.com>
2022-08-09 23:15:02 -0300

    PROYECTO FINAL

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+https://github.com/TestaJuani/react-coder/blob/master/src/imagenes/MuestraE-commerce.gif
+
 #Proyecto de React
 E-commerce, aqui tendras la oportunidad de ver todos nuestros productos, ademas de poder ingresar al detalle de cada uno de ellos y poder sumarlo al carrito de compras para una vez haber llenado el formulario de finalizacion de compra, obtener el producto en tu hogar.
--- src/components/ItemList/index.jsx
+++ src/components/ItemList/index.jsx
@@ -15,11 +15,11 @@ const ItemList = ({products}) => {
 
   return (
     <div className='container-itemList'>
-    {categoryProducto == "electronics" && <h1>Productos de electronica</h1>}
-    {categoryProducto == "jewelery" && <h1>Nuestras joyas</h1>}
-    {categoryProducto == "women's clothing" && <h1>Ropa de mujer</h1>}
-    {categoryProducto == "men's clothing" && <h1>Ropa de hombre</h1>}
-    {categoryProducto == "menu" && <h1>Nuestros productos</h1>}
+    {categoryProducto === "electronics" && <h1>Productos de electronica</h1>}
+    {categoryProducto === "jewelery" && <h1>Nuestras joyas</h1>}
+    {categoryProducto === "women's clothing" && <h1>Ropa de mujer</h1>}
+    {categoryProducto === "men's clothing" && <h1>Ropa de hombre</h1>}
+    {categoryProducto === "menu" && <h1>Nuestros productos</h1>}
     <div className='lista-productos'>
         {products.map(producto => {
             return  <Item product={producto} key={producto.id}/>
